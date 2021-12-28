117d3084dcfbbb30bec1e5596f8fad0b8485688d
ckstmznf <ckstmznf@naver.com>
2021-12-28 09:09:15 +0900

    Setup plugin

--- app/src/main/java/com/edcan/wishare/activity/main/MainActivity.kt
+++ app/src/main/java/com/edcan/wishare/activity/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.edcan.wishare
+package com.edcan.wishare.activity.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.edcan.wishare.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
